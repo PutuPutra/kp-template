702276f7ceac16fc9f2c31337d1651af924597f6
Putu Putra Eka Persada <83483873+PutuPutra@users.noreply.github.com>
2023-01-02 10:13:03 +0700

    new

--- app/Views/layout/templateAdmin.php
+++ app/Views/layout/templateAdmin.php
@@ -67,9 +67,15 @@
                         <li class="sidebar-title">Menu</li>
 
                         <li class="sidebar-item active ">
-                            <a href="index.html" class='sidebar-link'>
+                            <a href="dashboard" class='sidebar-link'>
                                 <i class="bi bi-grid-fill"></i>
                                 <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item ">
+                            <a href="input" class='sidebar-link'>
+                                <i class="fa-solid fa-plus"></i>
+                                <span>Input</span>
                             </a>
                         </li>
                         <!-- <li class="sidebar-item">
